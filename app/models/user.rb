677a6929bc79b596181a6fe0569e65321a5a766c
brugobi <brgobbi@hotmail.com>
2020-09-04 15:32:43 -0400

    add aticle model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :articles, class_name: 'Article', foreign_key: 'author_id'
+  #validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_04_182946) do
+ActiveRecord::Schema.define(version: 2020_09_04_184935) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.text "image"
+    t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
