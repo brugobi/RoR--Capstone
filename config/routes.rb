e987ac19261c6955ad52ba06d846c1a96d724508
brugobi <brgobbi@hotmail.com>
2020-09-07 17:10:41 -0400

    set up articles index as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :categories
   resources :articles
-  root 'pages#home'
+  #root 'pages#home'
+  root 'articles#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
