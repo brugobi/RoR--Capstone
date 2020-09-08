6d5ccd51639a9605f765fc775219ff8792c531be
brugobi <brgobbi@hotmail.com>
2020-09-08 13:39:59 -0400

    add images and categories in my feed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ Category.destroy_all
 # Imagem de Free-Photos por Pixabay
 # create a category first
 
-my_category = Category.create([{name: 'Outside', priority: true}, {name: 'Food', priority: true}, {name: 'City', priority: true}])
+my_category = Category.create([{name: 'Outside', priority: true}, {name: 'Food', priority: true}, {name: 'City', priority: true},{name: 'Sports', priority: true}, {name: 'Monument', priority: true}])
 
 p "Created #{Category.count} categories"
 
@@ -26,25 +26,161 @@ article = Article.create([
     title: Faker::Lorem.sentence(3, false, 0).chop,  
     content: Faker::Lorem.paragraphs, 
     category: my_category[0],
-    photo: File.open('app/assets/images/toronto.jpg')
+    # Imagem de 11417994 por Pixabay
+    image: 'https://cdn.pixabay.com/photo/2019/01/27/22/33/chipmunk-3959206_960_720.jpg'
+    #photo: File.open('app/assets/images/espresso.jpg')
+  },
+  {
+    title: Faker::Lorem.sentence(3, false, 0).chop,  
+    content: Faker::Lorem.paragraphs, 
+    category: my_category[0],
+    # Imagem de InsightPhotography por Pixabay 
+    image: 'https://cdn.pixabay.com/photo/2019/11/12/18/41/buildings-4621816_960_720.jpg'
+    #photo: File.open('app/assets/images/buildings.jpg')
+  },
+  {
+    title: Faker::Lorem.sentence(3, false, 0).chop,  
+    content: Faker::Lorem.paragraphs, 
+    category: my_category[0],
+    # Imagem de Jaime Reimer por Pixabay 
+    image: 'https://cdn.pixabay.com/photo/2019/08/02/23/09/man-4380804_960_720.jpg'
+    #photo: File.open('app/assets/images/braids.jpg')
+  },
+  {
+    title: Faker::Lorem.sentence(3, false, 0).chop,  
+    content: Faker::Lorem.paragraphs, 
+    category: my_category[0],
+    # Imagem de Free-Photos por Pixabay 
+    image: 'https://cdn.pixabay.com/photo/2014/08/21/09/28/brown-bear-423202_960_720.jpg'
+    #photo: File.open('app/assets/images/braids.jpg')
   },
   {
     title: Faker::Lorem.sentence(3, false, 0).chop,  
     content: Faker::Lorem.paragraphs, 
     category: my_category[1],
-    photo: File.open('app/assets/images/braids.jpg')
+    # Imagem de Free-Photos por Pixabay
+    image: 'https://cdn.pixabay.com/photo/2016/02/19/10/59/espresso-1209522_960_720.jpg'
+    #photo: File.open('app/assets/images/espresso.jpg')
+  },
+  {
+    title: Faker::Lorem.sentence(3, false, 0).chop,  
+    content: Faker::Lorem.paragraphs, 
+    category: my_category[1],
+    # Imagem de photosforyou por Pixabay
+    image: 'https://cdn.pixabay.com/photo/2016/12/14/22/14/skeleton-1907613_960_720.jpg'
+    #photo: File.open('app/assets/images/espresso.jpg')
+  },
+   {
+    title: Faker::Lorem.sentence(3, false, 0).chop,  
+    content: Faker::Lorem.paragraphs, 
+    category: my_category[1],
+    # Imagem de Sherry Galey por Pixabay 
+    image: 'https://cdn.pixabay.com/photo/2015/07/11/14/45/berries-840832_960_720.jpg'
+    #photo: File.open('app/assets/images/espresso.jpg')
+  },
+  {
+    title: Faker::Lorem.sentence(3, false, 0).chop,  
+    content: Faker::Lorem.paragraphs, 
+    category: my_category[1],
+    # Imagem de adamkontor por Pixabay
+    image: 'https://cdn.pixabay.com/photo/2018/10/21/20/35/lanzarote-3763773_960_720.jpg'
+    #photo: File.open('app/assets/images/espresso.jpg')
   },
   {
     title: Faker::Lorem.sentence(3, false, 0).chop,  
     content: Faker::Lorem.paragraphs, 
     category: my_category[2],
-    photo: File.open('app/assets/images/espresso.jpg')
+    # Imagem de My pictures are CC0. When doing composings: por Pixabay 
+    image: 'https://cdn.pixabay.com/photo/2018/01/27/23/46/toronto-3112508_960_720.jpg'
+    #photo: File.open('app/assets/images/toronto.jpg')
   },
   {
     title: Faker::Lorem.sentence(3, false, 0).chop,  
     content: Faker::Lorem.paragraphs, 
-    category: my_category[1],
-    photo: File.open('app/assets/images/buildings.jpg')
+    category: my_category[2],
+    # Imagem de Noel Bauza por Pixabay
+    image: 'https://cdn.pixabay.com/photo/2016/02/07/19/49/snow-1185469_960_720.jpg'
+    #photo: File.open('app/assets/images/espresso.jpg')
+  },
+  {
+    title: Faker::Lorem.sentence(3, false, 0).chop,  
+    content: Faker::Lorem.paragraphs, 
+    category: my_category[2],
+    # Imagem de Irene K-s por Pixabay 
+    image: 'https://cdn.pixabay.com/photo/2018/06/22/13/29/ottawa-3490766_960_720.jpg'
+    #photo: File.open('app/assets/images/toronto.jpg')
+  },
+  {
+    title: Faker::Lorem.sentence(3, false, 0).chop,  
+    content: Faker::Lorem.paragraphs, 
+    category: my_category[2],
+    # Imagem de Free-Photos por Pixabay
+    image: 'https://cdn.pixabay.com/photo/2015/03/26/10/00/montreal-690737_960_720.jpg'
+    #photo: File.open('app/assets/images/espresso.jpg')
+  },
+  {
+    title: Faker::Lorem.sentence(3, false, 0).chop,  
+    content: Faker::Lorem.paragraphs, 
+    category: my_category[3],
+    # Imagem de Ralf Schnabel por Pixabay
+    image: 'https://cdn.pixabay.com/photo/2017/01/12/08/09/heliskiing-heli-skiing-1974015_960_720.jpg'
+    #photo: File.open('app/assets/images/espresso.jpg')
+  },
+  {
+    title: Faker::Lorem.sentence(3, false, 0).chop,  
+    content: Faker::Lorem.paragraphs, 
+    category: my_category[3],
+    # Imagem de David Mark por Pixabay
+    image: 'https://cdn.pixabay.com/photo/2013/04/05/14/12/hockey-100729_960_720.jpg'
+    #photo: File.open('app/assets/images/espresso.jpg')
+  },
+  {
+    title: Faker::Lorem.sentence(3, false, 0).chop,  
+    content: Faker::Lorem.paragraphs, 
+    category: my_category[3],
+    # Imagem de Ri Butov por Pixabay 
+    image: 'https://cdn.pixabay.com/photo/2020/01/13/23/15/snowboarding-4763731_960_720.jpg'
+    #photo: File.open('app/assets/images/espresso.jpg')
+  },
+  {
+    title: Faker::Lorem.sentence(3, false, 0).chop,  
+    content: Faker::Lorem.paragraphs, 
+    category: my_category[3],
+    # Imagem de Keith Johnston por Pixabay 
+    image: 'https://cdn.pixabay.com/photo/2016/06/16/22/40/football-game-1462322_960_720.jpg'
+    #photo: File.open('app/assets/images/espresso.jpg')
+  },
+  {
+    title: Faker::Lorem.sentence(3, false, 0).chop,  
+    content: Faker::Lorem.paragraphs, 
+    category: my_category[4],
+    # Imagem de lumix2004 por Pixabay 
+    image: 'https://cdn.pixabay.com/photo/2016/11/30/15/00/lighthouse-1872998_960_720.jpg'
+    #photo: File.open('app/assets/images/espresso.jpg')
+  },
+  {
+    title: Faker::Lorem.sentence(3, false, 0).chop,  
+    content: Faker::Lorem.paragraphs, 
+    category: my_category[4],
+    # Imagem de Kranich17 por Pixabay 
+    image: 'https://cdn.pixabay.com/photo/2019/07/22/20/05/totem-pole-4355956_960_720.jpg'
+    #photo: File.open('app/assets/images/espresso.jpg')
+  },
+  {
+    title: Faker::Lorem.sentence(3, false, 0).chop,  
+    content: Faker::Lorem.paragraphs, 
+    category: my_category[4],
+    # Imagem de Aurore Duwez por Pixabay
+    image: 'https://cdn.pixabay.com/photo/2017/04/24/17/25/frontenac-2257154_960_720.jpg'
+    #photo: File.open('app/assets/images/espresso.jpg')
+  },
+  {
+    title: Faker::Lorem.sentence(3, false, 0).chop,  
+    content: Faker::Lorem.paragraphs, 
+    category: my_category[4],
+    # Imagem de Jeff Leonhardt por Pixabay
+    image: 'https://cdn.pixabay.com/photo/2016/08/13/07/20/niagara-1590345_960_720.jpg'
+    #photo: File.open('app/assets/images/buildings.jpg')
   }]
 )
 
