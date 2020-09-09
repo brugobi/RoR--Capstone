81529d3a719aaddadccaad51d2003a1e4ba9d5a0
brugobi <brgobbi@hotmail.com>
2020-09-09 16:53:28 -0400

    change the  messages at votescontroller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,9 +3,9 @@ class VotesController < ApplicationController
     @vote = current_user.votes.new(article_id: params[:article_id])
 
     if @vote.save
-      redirect_to articles_path, notice: 'You liked a post.'
+      redirect_to articles_path, notice: 'You upvoted a article.'
     else
-      redirect_to articles_path, alert: 'You cannot like this post.'
+      redirect_to articles_path, alert: 'You cannot upvote this article.'
     end
   end
 
@@ -13,9 +13,9 @@ class VotesController < ApplicationController
     vote = Vote.find_by(id: params[:id], user: current_user, article_id: params[:article_id])
     if vote
       vote.destroy
-      redirect_to articles_path, notice: 'You disliked a post.'
+      redirect_to articles_path, notice: 'You downvote a article.'
     else
-      redirect_to articles_path, alert: 'You cannot dislike post that you did not like before.'
+      redirect_to articles_path, alert: 'You cannot downvote article that you did not like before.'
     end
   end
 end
