f9ebab813cc4ea2c31e00d71b13229b9f2ef70d5
brugobi <brgobbi@hotmail.com>
2020-09-10 10:07:20 -0400

    add :index before action ate categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show, :edit, :update, :destroy]
+  before_action :set_category, only: [:index, :show, :edit, :update, :destroy]
 
   # GET /categories
   # GET /categories.json
