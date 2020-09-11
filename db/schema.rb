7dc671d9701e781e82676340c418317ae6855817
brugobi <brgobbi@hotmail.com>
2020-09-11 15:06:54 -0400

    add colum vote to user and articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_165940) do
+ActiveRecord::Schema.define(version: 2020_09_11_134843) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_09_09_165940) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
+    t.integer "votes_count", default: 0
     t.index ["category_id"], name: "index_articles_on_category_id"
   end
 
@@ -60,6 +61,7 @@ ActiveRecord::Schema.define(version: 2020_09_09_165940) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.integer "votes_count", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
