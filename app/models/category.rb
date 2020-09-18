38fc70016d81695c98f025163c89018e55571040
brugobi <brgobbi@hotmail.com>
2020-09-18 17:09:00 -0400

    add validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
-  has_many :articles
+  has_many :articles, -> { order(created_at: :desc) }
+  validates_uniqueness_of :name, case_sensitive: false
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :articles, class_name: 'Article', foreign_key: 'author_id'
-  # validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
   has_many :votes, dependent: :destroy
+  validates :name, presence: true, length: { maximum: 100 }
 end
