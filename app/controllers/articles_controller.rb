0d4922102d959ece2c9d10078ccc83aa50877877
brugobi <brgobbi@hotmail.com>
2020-09-28 13:26:25 -0400

    refactory the update/destroy methods and add links to the articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -43,25 +43,22 @@ class ArticlesController < ApplicationController
   # PATCH/PUT /articles/1.json
   def update
     @article.category_id = params[:category_id]
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    if @article.update(article_params)
+      flash[:notice] = 'Article updated successfully.'
+      redirect_to article_path(@article)
+    else
+      lash[:alert] = @article.errors.full_messages
+      redirect_back(fallback_location: new_article_path)
     end
   end
 
   # DELETE /articles/1
   # DELETE /articles/1.json
   def destroy
+    @article = current_user.articles.find(params[:id])
     @article.destroy
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    flash[:notice] = 'Article was successfully destroyed.'
+    redirect_to root_path
   end
 
   private
