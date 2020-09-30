d7c0b07ade34d3e14f7f3543cfcacde3a6cd7f3c
brugobi <brgobbi@hotmail.com>
2020-09-30 17:26:43 -0400

    refactoring the upvote method

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -7,7 +7,6 @@ class VotesController < ApplicationController
     else
       article_voted = Article.find(params[:article_id])
       current_user.voted_articles << article_voted
-      article_voted.update_attributes(votes_count: article_voted.votes_count + 1)
       flash[:notice] = 'You upvoted this article.'
     end
     redirect_back(fallback_location: root_path)
@@ -18,7 +17,6 @@ class VotesController < ApplicationController
     vote = Vote.find_by(article_id: params[:article_id], user_id: current_user.id)
     if vote
       vote.destroy
-      article_voted.update_attributes(votes_count: article_voted.votes_count - 1)
       flash[:alert] = 'You downvote a article'
     else
       flash[:alert] = 'You cannot downvote article that you did not like before.!'
--- app/models/article.rb
+++ app/models/article.rb
@@ -4,7 +4,6 @@ class Article < ApplicationRecord
   has_one_attached :photo
   belongs_to :category # class_name: 'Article', foreign_key: 'category_id'
   has_many :votes, dependent: :destroy
-
   scope :belong_to_category, -> { order(:title).limit(4) }
   scope :with_votes, -> { select('articles.*, COUNT(votes.user_id) as votes_count').left_joins(:votes).group(:id) }
   scope :most_voted, -> { with_votes.order(votes_count: :desc) }
@@ -12,4 +11,10 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100 }
   validates :photo, :category_id, presence: true
   validates :content, presence: true, length: { maximum: 300 }
+  
+  # Method that incrementes votes_count cache
+  def self.increment_vote_cache(article_id)
+    article = find(article_id)
+    article.update_attributes(votes_count: article.votes_count + 1)
+  end
 end
