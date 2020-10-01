29b2f1f8dda103916c80e0c80aa6cd983e41f0fb
brugobi <brgobbi@hotmail.com>
2020-10-01 13:56:22 -0400

    add dependent: :destroy to articles and votes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :articles, foreign_key: 'author_id'
+  has_many :articles, foreign_key: 'author_id', dependent: :destroy
   has_many :votes, dependent: :destroy
-  has_many :voted_articles, through: :votes, source: :article
+  has_many :voted_articles, through: :votes, source: :article, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 100 }
   validates :name, uniqueness: { case_sensitive: false }
