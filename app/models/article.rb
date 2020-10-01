5316895ca9a7ddcbe6b78d5e1054363a34756b8c
brugobi <brgobbi@hotmail.com>
2020-10-01 11:34:12 -0400

    add filter to sort the category articles by date desc

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,16 +2,18 @@ class Article < ApplicationRecord
   belongs_to :author, class_name: User.name
   belongs_to :user, optional: true # to fix a issue with: user must to exist
   has_one_attached :photo
-  belongs_to :category # class_name: 'Article', foreign_key: 'category_id'
+  belongs_to :category
   has_many :votes, dependent: :destroy
-  scope :belong_to_category, -> { order(:title).limit(4) }
+
+  scope :belong_to_category_by_date, -> { order(created_at: :desc) } 
+  scope :belong_to_category, -> { belong_to_category_by_date.order(:title).limit(4) } 
   scope :with_votes, -> { select('articles.*, COUNT(votes.user_id) as votes_count').left_joins(:votes).group(:id) }
   scope :most_voted, -> { with_votes.order(votes_count: :desc) }
 
   validates :title, presence: true, length: { maximum: 100 }
-  validates :photo, :category_id, presence: true
+  validates :image, :category_id, presence: true
   validates :content, presence: true, length: { maximum: 300 }
-  
+
   # Method that incrementes votes_count cache
   def self.increment_vote_cache(article_id)
     article = find(article_id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  # resources :votes, only: [:create, :destroy]
   resources :categories
-  # resources :articles
-  #root 'pages#home'
   root 'articles#index'
   devise_for :users
   resources :articles do
